38832d38c05a69b9603195ab310f573cd87ad063
HADIFZAKWANBINAHMADFRIDAUS <hadif.zakwan@gmail.com>
2026-01-20 10:23:16 +0800

    NEW

---
+++ Biodata.php
@@ -0,0 +1,13 @@
+<?php
+$name = "HADIF ZAKWAN";
+$nomatrik = "18DIT24F1071";
+$kelas = "DIT4B";
+
+
+<h1>Biodata Saya</h1>
+
+<img src="hadae.jpeg" alt="Gambar Profil" class="profile-pic">
+
+<p><b>Nama:</b> <span><?= $name ?></span></p>
+<p><b>No Matrik:</b> <span><?= $nomatrik ?></span></p>
+<p><b>Kelas:</b> <span><?= $kelas ?></span></p>
